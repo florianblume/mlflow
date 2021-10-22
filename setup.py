c50c7fbb32cba14db21cf2ad9fbb3d492b53b8ea
Florian Blume <flob.91@gmx.de>
2021-10-22 16:00:55 +0200

    Added dependencies for docker and singularity

--- setup.py
+++ setup.py
@@ -51,6 +51,8 @@ SKINNY_REQUIREMENTS = [
     "pytz",
     "requests>=2.17.3",
     "packaging",
+    "docker",
+    "singularity",
     # Automated dependency detection in MLflow Models relies on
     # `importlib_metadata.packages_distributions` to resolve a module name to its package name
     # (e.g. 'sklearn' -> 'scikit-learn'). importlib_metadata 3.7.0 or newer supports this function:
